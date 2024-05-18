8f711c2d4e975afe00499f58043dfe42a142cda1
Mr-Keks <levelel744@gmail.com>
2024-05-18 15:02:17 +0200

    added saving time following placeholder hours:minutes:seconds

--- utilities/create_task.py
+++ utilities/create_task.py
@@ -1,6 +1,6 @@
 from objects.task import Task
 from utilities import save_data
-from utilities.validation import time_format
+from utilities.validation import str_time_format, int_time_format
 
 
 def create_task(task_name, time_limited, amount_of_time, start_position):
@@ -13,15 +13,15 @@ def create_task(task_name, time_limited, amount_of_time, start_position):
     task.set_time_limited(time_limited)
 
     if time_limited:
-        task.set_amount_of_time(amount_of_time)
+        task.set_amount_of_time(str_time_format(amount_of_time))
         if start_position:
             # stopwatch starts from the end
             task.set_reverse_time()
             task.set_task_destination(0)
-            task.set_last_saved_time(time_format(amount_of_time))
+            task.set_last_saved_time(int_time_format(amount_of_time))
         else:
             # stopwatch start from the beginnig
-            task.set_task_destination(time_format(amount_of_time))
+            task.set_task_destination(int_time_format(amount_of_time))
             task.set_last_saved_time(0)
     
     save.save_task(task)
--- utilities/validation.py
+++ utilities/validation.py
@@ -4,7 +4,25 @@ def number_validation(number):
     '''
     return number.isdigit
 
-def time_format(str_time):
+def str_time_format(str_time):
+    '''
+        time display: hours:minutes:seconds
+        examples: 100:00:00; 00:45:00
+    '''
+
+    hours, minutes = str_time.split(":")
+
+    def add_zero(time):
+        '''
+            adding zero prefix
+        '''
+        if len(time) == 1:
+            time = "0"+time
+        return time
+    
+    return ":".join([add_zero(hours), add_zero(minutes), "00"])
+
+def int_time_format(str_time):
     hours, minutes = str_time.split(":")
    
     return int(hours)*60 + int(minutes)
